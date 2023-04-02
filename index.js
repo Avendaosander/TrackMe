ab2e183488aae70ea8b98f9da4a71520560ed32e
Alexander <avendano.ramirez@gmail.com>
2023-04-01 22:49:59 -0400

    Sweet Alert y boton de ver datos agregados

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ console.log("Bienvenido a TrackMe");
 const URL_API_IP = "https://api.ipify.org/?format=json";
 
 // APIs de https://geo.ipify.org/docs
-const URL_API_GEO ="https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_tJNUdet4sUYU6WfaWQqCyHi0UYsGG&ipAddress=";
-const URL_API_GEO_DOMAIN ="https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_tJNUdet4sUYU6WfaWQqCyHi0UYsGG&domain=";
+const URL_API_GEO ="https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_DL5bwsg9d1oydbAtuR1v131e9SAgP&ipAddress=";
+const URL_API_GEO_DOMAIN ="https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_DL5bwsg9d1oydbAtuR1v131e9SAgP&domain=";
 
 // Aqui se guarda la IP del usuario
 let IP = "";
@@ -56,6 +56,9 @@ const ejemplos = {
    }
 }
 
+// Aqui se guarda los datos de la dirección
+let infoUser = {}
+
 // Formulario
 let formulario = document.getElementById("form");
 
@@ -64,6 +67,9 @@ let coordenadas = document.getElementById("coordenadas");
 
 // Boton para buscar direccion por Coordenadas
 let sendCoordenadas = document.getElementById("sendCoordenadas");
+
+// Boton para mostrar los datos de la busqueda
+let showData = document.getElementById('showData')
 
 // Aqui se guarda la etiqueta body
 const body = document.querySelector("body")
@@ -74,11 +80,11 @@ const buttomTheme = document.getElementById('switch-theme')
 // Al cargar la pagina se obtiene la IP y la Geolocalizacion del usuario
 document.addEventListener("DOMContentLoaded", async () => {
    await obtenerIp();
-   if (!IP) return alert('La peticion no se pudo completar, revise su conexión a internet y vuelva a intentarlo');
+   if (!IP) return alertError();
    // console.log(IP);
    
    await obtenerGeo(IP)
-   if (!Geo) return alert('La peticion no se pudo completar, revise su conexión a internet y vuelva a intentarlo')
+   if (!Geo) return alertError()
    // console.log(Geo);
 
    document.getElementById("loader").classList.toggle('loader-off')
@@ -87,6 +93,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
    contPopup = await crearPopup(Geo)
    // console.log(contPopup);
+   buttonShowData()
 
    marcador.remove()
    await GenerarMarcador(Geo.location.lat, Geo.location.lng, contPopup)
@@ -121,7 +128,7 @@ const obtenerGeoDominio = async (domain) => {
          .then(res => res.json())
          .then(res => Geo = res)
    } catch (error) {
-      console.error(error.message)
+      console.error(error.message);
    }
 };
 
@@ -141,15 +148,24 @@ GenerarMarcador(latitud, longitud, "Venezuela")
 
 // Funcion para crar un popup con la información del usuario
 const crearPopup = (location) => {
+   infoUser = {
+      ip: location.ip,
+      domain: location.as ? location.as.domain : location.domains[0],
+      country: location.location.country,
+      region: location.location.region,
+      city: location.location.city,
+      lat: location.location.lat,
+      lon: location.location.lng
+   }
    contPopup = document.createRange().createContextualFragment(/*html*/
       `<div id="contPopup">
-         <p class="msg-popup">Ip: ${location.ip}</p>
-         <p class="msg-popup">Domain: ${location.as ? location.as.domain : location.domains[0]}</p>
-         <p class="msg-popup">Location</p>
-         <p class="msg-popup">Country: ${location.location.country}</p>
-         <p class="msg-popup">Region: ${location.location.region}</p>
-         <p class="msg-popup">City: ${location.location.city}</p>
-         <p class="msg-popup">Coordinates: ${location.location.lat}, ${location.location.lng}</p>
+         <p class="msg-popup">Ip: ${infoUser.ip}</p>
+         <p class="msg-popup">Domain: ${infoUser.domain}</p>
+         <p class="msg-popup">🗺️Location⬇️</p>
+         <p class="msg-popup">Country: ${infoUser.country}</p>
+         <p class="msg-popup">Region: ${infoUser.region}</p>
+         <p class="msg-popup">City: ${infoUser.city}</p>
+         <p class="msg-popup">Coordinates: ${infoUser.lat}, ${infoUser.lon}</p>
       </div>`
    )
    return contPopup
@@ -190,15 +206,17 @@ btnCoordinate.addEventListener("click", e => {
       e.preventDefault();
       const regex = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*[,]\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
       if (regex.test(coordenadas.value.trim())) {
+         infoUser = {}
+         formulario.lastChild.remove()
+         showData = document.getElementById('showData')
          let coordinates = coordenadas.value.split(",");
          const lat = coordinates[0]
          const lng = coordinates[1]
          map.flyTo(coordinates, 18);
          marcador.remove()
-         contPopup = crearPopup(Geo)
          GenerarMarcador(lat, lng)
       } else {
-         return console.warn("No es una coordenada valida");
+         return Swal.fire('Un momento!', 'Las coordenadas ingresadas no son validas', 'warning')
       }
    });
 })
@@ -214,12 +232,14 @@ btnIp.addEventListener("click", e => {
    // Evento del boton al buscar una ubicacion por IP
    sendIp.addEventListener("click", async (e) => {
       e.preventDefault();
-      Geo = ''
       const regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
       const newIP = IPs.value.trim()
       if (regex.test(newIP)) {
+         infoUser = {}
+         Geo = ''
+         if(showData === null) buttonShowData()
          await obtenerGeo(newIP)
-         if (!Geo) return alert('La peticion no se pudo completar, revise su conexión a internet y vuelva a intentarlo')
+         if (!Geo) return alertError()
          const lat = Geo.location.lat
          const lng = Geo.location.lng
          map.flyTo([lat, lng], 18);
@@ -227,7 +247,7 @@ btnIp.addEventListener("click", e => {
          contPopup = crearPopup(Geo)
          GenerarMarcador(lat, lng, contPopup)
       } else {
-         return console.warn("No es una IP valida");
+         return Swal.fire('Un momento!', 'La IP ingresada no es valida', 'warning')
       }
    });
 })
@@ -243,12 +263,14 @@ btnDomain.addEventListener("click", e => {
    // Evento del boton al buscar una ubicacion por Dominio
    sendDominio.addEventListener("click", async (e) => {
       e.preventDefault();
-      Geo = ''
       const regex = /^[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
       const newDomain = dominio.value.trim()
       if (regex.test(newDomain)) {
+         infoUser = {}
+         Geo = ''
+         if(showData === null) buttonShowData()
          await obtenerGeoDominio(newDomain)
-         if (!Geo) return alert('La peticion no se pudo completar, revise su conexión a internet y vuelva a intentarlo')
+         if (!Geo) return alertError()
          const lat = Geo.location.lat
          const lng = Geo.location.lng
          map.flyTo([lat, lng], 18);
@@ -256,7 +278,7 @@ btnDomain.addEventListener("click", e => {
          contPopup = crearPopup(Geo)
          GenerarMarcador(lat, lng, contPopup)
       } else {
-         return console.warn("No es dominio valido");
+         return Swal.fire('Un momento!', 'El Dominio ingresado no es valido', 'warning');
       }
    });
 })
@@ -320,7 +342,7 @@ const filtrarBusqueda = (filtro) => {
          boton.id = 'sendDominio'
          break;
    }
-   boton.classList.add('btnForm')
+   boton.classList.add('btn','btnForm')
    boton.textContent = 'Buscar'
 
    while(formulario.firstChild){
@@ -331,6 +353,8 @@ const filtrarBusqueda = (filtro) => {
    formulario.appendChild(input);
    formulario.appendChild(boton);
 
+   if (infoUser.ip) buttonShowData()
+
    coordenadas = document.getElementById("coordenadas");
 }
 
@@ -339,6 +363,9 @@ sendCoordenadas.addEventListener("click", async (e) => {
    e.preventDefault();
    const regex = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*[,]\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
    if (regex.test(coordenadas.value.trim())) {
+      infoUser = {}
+      formulario.lastChild.remove()
+      showData = document.getElementById('showData')
       let coordinates = coordenadas.value.split(",");
       const lat = coordinates[0]
       const lng = coordinates[1]
@@ -346,7 +373,7 @@ sendCoordenadas.addEventListener("click", async (e) => {
       marcador.remove()
       GenerarMarcador(lat, lng)
    } else {
-      return console.warn("No es una coordenada valida");
+      return Swal.fire('Un momento!', 'Las coordenadas ingresadas no son validas', 'warning')
    }
 });
 
@@ -382,4 +409,48 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
    addLigthMode()
 } else{
    addDarkMode()
+}
+
+// Funcion para crear el boton de mostrar datos de la busqueda
+const buttonShowData = () => {
+   const buttonData = document.createElement('button')
+   buttonData.classList.add('btn', 'btn-data')
+   buttonData.id = "showData"
+   buttonData.textContent = 'Datos'
+
+   formulario.appendChild(buttonData)
+   showData = document.getElementById('showData')
+
+   // Evento para mostrar los datos de la busqueda
+   showData.addEventListener('click', e => {
+      e.preventDefault()
+      if(!infoUser.ip) return Swal.fire('Error', 'No hay datos para mostrar', 'error')
+      showAlert(infoUser)
+   })
+}
+
+// funcion para crear la alerta con la información de la busqueda
+function showAlert(data) {
+   Swal.fire({
+      icon: 'info',
+      title: 'Datos',
+      html: 
+         `
+            <div>
+               <p>Ip: ${data.ip}</p>
+               <p>Domain: ${data.domain}</p>
+               <p>🗺️Location⬇️</p>
+               <p>Country: ${data.country}</p>
+               <p>Region: ${data.region}</p>
+               <p>City: ${data.city}</p>
+               <p>Coordinates: ${data.lat}, ${data.lon}</p>
+            </div>
+         `,
+      showCloseButton: true,
+   })
+}
+
+// Función para crear la alerta de error en la peticion
+function alertError(){
+   Swal.fire('Error', 'La peticion no se pudo completar, revise su conexión a internet y vuelva a intentarlo', 'error')
 }
